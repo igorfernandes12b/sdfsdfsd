ddb0a7d8243f443f8d140c3e3d6e80d95e579ea0
igorfernandes12b <163745225+igorfernandes12b@users.noreply.github.com>
2025-05-30 23:37:53 -0300

    script.js

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
-const botaoAbrir = document.querySelector("header > button")
-const botaoFechar = document.querySelector("header nav button")
-const body = document.querySelector("body")
-const nav = document.querySelector(" header .mobile nav")
+const botaoAbrir = document.querySelector("header > button");
+const botaoFechar = document.querySelector("header nav button");
 
-botaoAbrir.addEventListener("click", abrirMenu)
-botaoFechar.addEventListener("click", fecharMenu)
+const body = document.querySelector("body");
+const nav = document.querySelector("header .mobile");
 
-function abrirMenu(){
-    body.classList.add("escurecer")
-    nav.classList.add("abrir")
+
+botaoAbrir.addEventListener("click", abrirMenu);
+botaoFechar.addEventListener("click", fecharMenu);
+
+
+function abrirMenu() {
+  body.classList.add("escurecer");
+  nav.classList.add("abrir");
 }
 
-function fecharMenu(){
-    body.classList.remove("escurecer")
-    nav.classList.remove("abrir")
+function fecharMenu() {
+  body.classList.remove("escurecer");
+  nav.classList.remove("abrir");
 }
-
- 

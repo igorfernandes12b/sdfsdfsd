ed720d60ebf4f9517546880aabcaecb9e68f6c79
igorfernandesrodrigues <igor.fernandes.jogos@gmail.com>
2025-05-30 23:36:15 -0300

    ultimo commit

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ const botaoFechar = document.querySelector("header nav button");
 const body = document.querySelector("body");
 const nav = document.querySelector("header .mobile");
 
+
 botaoAbrir.addEventListener("click", abrirMenu);
 botaoFechar.addEventListener("click", fecharMenu);
+
 
 function abrirMenu() {
   body.classList.add("escurecer");
